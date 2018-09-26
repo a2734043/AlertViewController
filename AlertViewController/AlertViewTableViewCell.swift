ac794ab06509382cd6364bbb5c1595fe7cd9a42c
a2734043 <a2734043@gmail.com>
2018-09-26 20:30:26 +0800

    first commit

---
+++ AlertViewController/AlertViewTableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  AlertViewTableViewCell.swift
+//  AlertViewController
+//
+//  Created by 陳鍵群 on 2018/9/26.
+//  Copyright © 2018年 陳鍵群. All rights reserved.
+//
+
+import UIKit
+
+class AlertViewTableViewCell: UITableViewCell {
+    @IBOutlet weak var label: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ AlertViewController/TableViewController.swift
@@ -0,0 +1,51 @@
+//
+//  TableViewController.swift
+//  RealmTest
+//
+//  Created by 陳鍵群 on 2018/9/20.
+//  Copyright © 2018年 陳鍵群. All rights reserved.
+//
+
+import UIKit
+
+class TableViewControllerInAlertView:UITableViewController{
+    var cells:[String] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        var size:CGSize?
+        
+        
+        
+        
+        
+        if (cells.count < 4) {
+            size = CGSize(width: 272, height: 100)
+        }else if (cells.count < 6){
+            size = CGSize(width: 272, height: 150)
+        }else if (cells.count < 8){
+            size = CGSize(width: 272, height: 200)
+        }else{
+            size = CGSize(width: 272, height: 250)
+        }
+        self.preferredContentSize = size!
+        
+        self.tableView.register(UINib(nibName: "AlertViewTableViewCell", bundle: nil), forCellReuseIdentifier: "Cell")
+        
+    }
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 45
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return cells.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell:AlertViewTableViewCell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! AlertViewTableViewCell
+        cell.label.text = "\(indexPath.row)"
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        self.dismiss(animated: true, completion: nil)
+    }
+}
